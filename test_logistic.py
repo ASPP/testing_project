69016b4fd8f41d17559ea3a897c61534f918cb5f
ASPP Student <student@localhost>
2023-08-29 14:40:53 +0300

    Add test for iteraterate_f

--- test_logistic.py
+++ test_logistic.py
@@ -1,7 +1,7 @@
 import pytest
 from numpy.testing import assert_allclose
 
-from logistic import f
+from logistic import f, iterate_f
 
 cases = [
         (0.1, 2.2, 0.198),
@@ -22,3 +22,14 @@ cases = [
 def test_f_corner_cases(x, r, expected):
     result = f(x, r)
     assert_allclose(result, expected)
+
+
+cases = [
+        (1, 0.1, 2.2, [0.1, 0.198]),
+        (4, 0.2, 3.4, [0.2, 0.544, 0.843418, 0.449019, 0.841163]),
+        (3, 0.5, 2, [0.5, 0.5, 0.5, 0.5])
+    ]
+@pytest.mark.parametrize('it, x, r, expected', cases)
+def test_iterate_f(it, x, r, expected):
+    result = iterate_f(it, x, r)
+    assert_allclose(result, expected, atol=0.000001)
