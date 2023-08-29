2dbbd40b3f3e7f2129eecab5c6ddb9eb599dae2b
ASPP Student <student@localhost>
2023-08-29 12:54:14 +0300

    Add iterate_f and corresponding test

--- logistic.py
+++ logistic.py
@@ -1,5 +1,14 @@
+from plot_logistic import plot_trajectory
+
 def f(x, r):
     """
     Implements the logistic function.
     """
     return r * x * (1 - x)
+
+def iterate_f(x, r, it):
+    traj = [x]
+    for _ in range(it):
+        x = f(x, r)
+        traj.append(x)
+    return traj
--- test_logistic.py
+++ test_logistic.py
@@ -1,6 +1,6 @@
 import pytest
 from numpy.testing import assert_allclose
-from logistic import f
+from logistic import f, iterate_f
 
 @pytest.mark.parametrize('x, r, expected', [
         (0, 1.1, 0),
@@ -20,3 +20,14 @@ def test_f_corner_cases(x, r, expected):
 def test_f_generic_cases(x, r, expected):
     result = f(x, r)
     assert_allclose(result, expected)
+
+
+@pytest.mark.parametrize('x, r, it, expected', [
+    (0.1, 2.2, 1, [0.1, 0.198]),
+    (0.2, 3.4, 4, [0.2, 0.544, 0.843418, 0.449019, 0.841163]),
+    (0.5, 2, 3, [0.5, 0.5, 0.5, 0.5]),
+    ]
+)
+def test_iterate_f(x, r, it, expected):
+    result = iterate_f(x, r, it)
+    assert_allclose(result, expected, rtol=5e-07)
