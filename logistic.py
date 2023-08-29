d925493648fcf1b94e9574a0fa299108a8517a03
ASPP Student <student@localhost>
2023-08-29 14:40:26 +0300

    add iterate_f

--- logistic.py
+++ logistic.py
@@ -1,4 +1,10 @@
 # Your code goes here
 def f(x,r):
     return r*x*(1-x)
-    
+
+def iterate_f(it,x,r):
+    results = [x]
+    for _ in range(it):
+        x = f(x, r)
+        results.append(x)
+    return results
--- test_logistic.py
+++ test_logistic.py
@@ -1,6 +1,7 @@
 from numpy.testing import assert_allclose
 import pytest
 from logistic import f
+from logistic import iterate_f
 
 # Add here your test for the logistic map
 
@@ -16,3 +17,13 @@ def test_f_corner_cases(x,r,expected):
 def test_f_generic_cases(x,r,expected):
     result = f(x, r)
     assert_allclose(result, expected)
+
+
+@pytest.mark.parametrize('it, x, r, expected', [
+    (1, 0.1, 2.2, [0.1, 0.198]),
+    (4, 0.2, 3.4, [0.2, 0.544, 0.843418, 0.449019, 0.841163]),
+    (3, 0.5, 2, [0.5, 0.5, 0.5, 0.5]),
+])
+def test_iterate_f(it,x,r,expected):
+    result = iterate_f(it, x, r)
+    assert_allclose(result, expected, atol=0.00001)
