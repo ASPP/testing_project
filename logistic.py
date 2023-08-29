adc62e11f7543076a3b206693b8f14842fba2be0
ASPP Student <student@localhost>
2023-08-29 12:04:25 +0300

    add logistic function, add new tests

--- logistic.py
+++ logistic.py
@@ -1,1 +1,7 @@
 # Your code goes here
+
+def f(x, r):
+
+    return r*x*(1-x)
+
+
--- test_logistic.py
+++ test_logistic.py
@@ -14,3 +14,15 @@ def test_f_corner_cases():
     for x, r, expected in cases:
         result = f(x, r)
         assert_allclose(result, expected)
+
+def test_f_generic_cases():
+    # Test cases are (x, r, expected)
+    cases = [
+        (0.1, 2.2, 0.198),
+        (0.2, 3.4, 0.544),
+        (0.5, 2, 0.5)
+    ]
+    for x, r, expected in cases:
+        result = f(x, r)
+        assert_allclose(result, expected)
+
