90111e4dcc338c90becb9ec17c87bffa1195f6e6
ASPP Student <student@localhost>
2023-08-29 15:13:07 +0300

    add test for convergence of iterate_f

--- test_logistic.py
+++ test_logistic.py
@@ -2,6 +2,9 @@ from numpy.testing import assert_allclose
 from logistic import f, iterate_f
 import pytest
 from math import isclose
+import numpy as np
+
+SEED = 42
 # Add here your test for the logistic map
 
 # def test_f_general():
@@ -33,7 +36,20 @@ from math import isclose
 
 
 
-@pytest.mark.parametrize('x,r,it,expected',[(0.1,2.2,1,[0.1,0.198]),(0.2,3.4,4,[0.2,0.544,0.843418,0.449019,0.841163]),(0.5,2,3,[0.5,0.5,0.5,0.5])])
-def test_iterate_f(x,r,it,expected):
-    result = iterate_f(x,r,it)
-    assert_allclose(result,expected,atol=0.001)
+# @pytest.mark.parametrize('x,r,it,expected',[(0.1,2.2,1,[0.1,0.198]),(0.2,3.4,4,[0.2,0.544,0.843418,0.449019,0.841163]),(0.5,2,3,[0.5,0.5,0.5,0.5])])
+# def test_iterate_f(x,r,it,expected):
+#     result = iterate_f(x,r,it)
+#     assert_allclose(result,expected,atol=0.001)
+
+def test_random_starting_point_convergence():
+    random_state = np.random.RandomState(SEED)
+    r = 1.5
+    n = 100
+    it = 30
+    n_convergence_datapoints = 10
+    for _ in range(n):
+        x0 = random_state.uniform(0.0001, 0.9999)
+        xs = iterate_f(x0,r,it)
+        for i in range(1, n_convergence_datapoints+1):
+            assert np.isclose(xs[-i], 1/3, atol=0.001)
+        
