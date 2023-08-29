234895c94037f97690ab069b159650f199cd8dbe
ASPP Student <student@localhost>
2023-08-29 12:09:50 +0300

    Improve functions and add tests for generic cases

--- logistic.py
+++ logistic.py
@@ -1,1 +1,10 @@
-# Your code goes here
+def logistic_step(x, r):
+    """ Compute the logistic map for a given value of x and r"""
+    return r * x * (1 - x)
+
+def run_iterations(start, n_iter, r):
+    """ Run iterations of logistic function """
+    val = [start]
+    for i_iter in range(n_iter):
+        val.append(logistic_step(val[i_iter], r))
+    return val
--- test_logistic.py
+++ test_logistic.py
@@ -1,16 +1,27 @@
 from numpy.testing import assert_allclose
 
-from logistic import f
+from logistic import logistic_step
 
 # Add here your test for the logistic map
 
 
-def test_f_corner_cases():
+def test_logistic_step_corner_cases():
     # Test cases are (x, r, expected)
     cases = [
         (0, 1.1, 0),
         (1, 3.7, 0),
     ]
     for x, r, expected in cases:
-        result = f(x, r)
+        result = logistic_step(x, r)
+        assert_allclose(result, expected)
+
+def test_logistic_step_generic_cases():
+    # Test cases are (x, r, expected)
+    cases = [
+        (0.1, 2.2, 0.198),
+        (0.2, 3.4, 0.544),
+        (0.5, 2, 0.5),
+    ]
+    for x, r, expected in cases:
+        result = logistic_step(x, r)
         assert_allclose(result, expected)
