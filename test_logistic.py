4583639aea5e1afc9dedac555067bd2fa7c373ff
ASPP Student <student@localhost>
2023-08-29 15:18:05 +0300

    Test convergence values using random initial values

--- test_logistic.py
+++ test_logistic.py
@@ -2,7 +2,8 @@ from numpy.testing import assert_allclose
 import pytest
 from logistic import logistic_step, run_iterations
 from logistic_fit import fit_r
-
+from itertools import product
+import numpy as np
 
 @pytest.mark.parametrize('x, r, expected', [
         (0, 1.1, 0),
@@ -33,12 +34,18 @@ def test_logistic_iterations(x, r, n_iter, expected):
     result = run_iterations(x, r, n_iter)
     assert_allclose(result, expected, rtol=1e-4)
 
-@pytest.mark.parametrize('x, r, n_iter', [
-        (0.1, 2.2, 1),
-        (0.2, 3.4, 4),
-        (0.5, 2, 3),
-    ]
-    )
-def test_logistic_fit_r(x, r, n_iter):
-    result = fit_r(run_iterations(x, r, n_iter))
-    assert_allclose(result, r, rtol=1e-4)
+
+
+
+# @pytest.mark.parametrize('x, r, n_iter', [(i/10,f/10,20) for i,f in product(range(0,20),range(0,30,5))]
+#     )
+# def test_logistic_fit_r(x, r, n_iter):
+#     result = fit_r(run_iterations(x, r, n_iter))
+#     assert_allclose(result, r, rtol=1e-3)
+SEED = 5
+@pytest.mark.parametrize('x', [np.random.RandomState(SEED).uniform(0.0001, 0.9999) for _ in range(50)])
+def test_logistic_convergence(x):
+    r = 1.5
+    n_iter = 100
+    result = run_iterations(x, r, n_iter)
+    assert_allclose(result[-1], 1/3, rtol=1e-4)
