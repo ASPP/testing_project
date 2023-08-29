56d6f4f44eb85cee3a5ab8d6e75ea01ae2604b1b
ASPP Student <student@localhost>
2023-08-29 12:26:31 +0300

    Use parametrize for tests

--- test_logistic.py
+++ test_logistic.py
@@ -1,24 +1,24 @@
+import pytest
 from numpy.testing import assert_allclose
 
 from logistic import f
 
-def test_generic_cases():
-    cases = [
+cases = [
         (0.1, 2.2, 0.198),
         (0.2, 3.4, 0.544),
         (0.5, 2.0, 0.5)
     ]
-    for x, r, expected in cases:
-        result = f(x, r)
-        assert_allclose(result, expected) 
+@pytest.mark.parametrize('x, r, expected', cases)
+def test_generic_cases(x, r, expected):
+    result = f(x, r)
+    assert_allclose(result, expected) 
 
 
-def test_f_corner_cases():
-    # Test cases are (x, r, expected)
-    cases = [
+cases = [
         (0, 1.1, 0),
         (1, 3.7, 0),
     ]
-    for x, r, expected in cases:
-        result = f(x, r)
-        assert_allclose(result, expected)
+@pytest.mark.parametrize('x, r, expected', cases)
+def test_f_corner_cases(x, r, expected):
+    result = f(x, r)
+    assert_allclose(result, expected)
