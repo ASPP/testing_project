4fb0943b62e96dd4cc37d4e7e86025676b63d3d6
ASPP Student <student@localhost>
2023-08-29 12:30:57 +0300

    Parametrize corner and generic cases

--- test_logistic.py
+++ test_logistic.py
@@ -1,8 +1,10 @@
 from numpy.testing import assert_allclose
 
 from logistic import f
+import pytest
 
 # Add here your test for the logistic map
+
 
 
 def test_f_corner_cases():
@@ -25,3 +27,25 @@ def test_f_generic_cases():
     for x, r, expected in cases:
         result = f(x, r)
         assert_allclose(result, expected)
+
+@pytest.mark.parametrize('x,r,expected',[
+        (0.1, 2.2, 0.198),
+        (0.2, 3.4, 0.544),
+        (0.5, 2, 0.5)
+    ])
+
+def test_f_generic_cases_param(x,r,expected):
+    # Test cases are (x, r, expected)
+
+    result = f(x,r)
+    assert_allclose(result, expected)
+
+
+@pytest.mark.parametrize('x,r,expected',[
+        (0, 1.1, 0),
+        (1, 3.7, 0),
+    ])
+def test_f_generic_corner_param(x,r,expected):
+    # Test cases are (x, r, expected)
+    result = f(x,r)
+    assert_allclose(result, expected)
