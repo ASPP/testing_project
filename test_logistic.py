e0f1bdd991b371583268e32ed19336e0d725250c
ASPP Student <student@localhost>
2023-08-29 12:27:31 +0300

    parametrize

--- test_logistic.py
+++ test_logistic.py
@@ -1,26 +1,32 @@
 from numpy.testing import assert_allclose
-
 from logistic import f
-
+import pytest
+from math import isclose
 # Add here your test for the logistic map
 
-def test_f_general():
-    cases = [
-        (0.1, 2.2, 0.198),
-        (0.2, 3.4, 0.544),
-        (0.5, 2, 0.5),
-    ]
-    for x, r, expected in cases:
-        result = f(x, r)
-        assert_allclose(result, expected)
+# def test_f_general():
+#     cases = [
+#         (0.1, 2.2, 0.198),
+#         (0.2, 3.4, 0.544),
+#         (0.5, 2, 0.5),
+#     ]
+#     for x, r, expected in cases:
+#         result = f(x, r)
+#         assert_allclose(result, expected)
 
 
-def test_f_corner_cases():
-    # Test cases are (x, r, expected)
-    cases = [
-        (0, 1.1, 0),
-        (1, 3.7, 0),
-    ]
-    for x, r, expected in cases:
-        result = f(x, r)
-        assert_allclose(result, expected)
+# def test_f_corner_cases():
+#     # Test cases are (x, r, expected)
+#     cases = [
+#         (0, 1.1, 0),
+#         (1, 3.7, 0),
+#     ]
+#     for x, r, expected in cases:
+#         result = f(x, r)
+#         assert_allclose(result, expected)
+
+
+@pytest.mark.parametrize('x,r,expected',[(0.1,2.2,0.198),(0.2,3.4,0.544),(0.5,2,0.5),(0,1.1,0),(1,3.7,0)])
+def test_f(x,r,expected):
+    result = f(x,r)
+    assert isclose(result,expected)
