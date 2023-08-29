ab5a495199bd5fa81e96039aa67d325a323ee864
ASPP Student <student@localhost>
2023-08-29 12:59:10 +0300

    plotting function

--- test_logistic.py
+++ test_logistic.py
@@ -1,5 +1,5 @@
 from numpy.testing import assert_allclose
-from logistic import f
+from logistic import f, iterate_f
 import pytest
 from math import isclose
 # Add here your test for the logistic map
@@ -26,7 +26,14 @@ from math import isclose
 #         assert_allclose(result, expected)
 
 
-@pytest.mark.parametrize('x,r,expected',[(0.1,2.2,0.198),(0.2,3.4,0.544),(0.5,2,0.5),(0,1.1,0),(1,3.7,0)])
-def test_f(x,r,expected):
-    result = f(x,r)
-    assert isclose(result,expected)
+# @pytest.mark.parametrize('x,r,expected',[(0.1,2.2,0.198),(0.2,3.4,0.544),(0.5,2,0.5),(0,1.1,0),(1,3.7,0)])
+# def test_f(x,r,expected):
+#     result = f(x,r)
+#     assert isclose(result,expected)
+
+
+
+@pytest.mark.parametrize('x,r,it,expected',[(0.1,2.2,1,[0.1,0.198]),(0.2,3.4,4,[0.2,0.544,0.843418,0.449019,0.841163]),(0.5,2,3,[0.5,0.5,0.5,0.5])])
+def test_iterate_f(x,r,it,expected):
+    result = iterate_f(x,r,it)
+    assert_allclose(result,expected,atol=0.001)
