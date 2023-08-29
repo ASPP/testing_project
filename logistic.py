6807cba95514c1a26df1606e34e0d6605b50204f
ASPP Student <student@localhost>
2023-08-29 12:42:19 +0300

    Test growth trajectories; reorder variables

--- logistic.py
+++ logistic.py
@@ -3,7 +3,7 @@ def logistic_step(x, r):
     constant_val = 1
     return r * x * (constant_val - x)
 
-def run_iterations(start, n_iter, r):
+def run_iterations(start, r, n_iter):
     """ Run iterations of logistic function """
     val = [start]
     for i_iter in range(n_iter):
--- test_logistic.py
+++ test_logistic.py
@@ -1,6 +1,6 @@
 from numpy.testing import assert_allclose
 import pytest
-from logistic import logistic_step
+from logistic import logistic_step, run_iterations
 
 
 @pytest.mark.parametrize('x, r, expected', [
@@ -21,3 +21,13 @@ def test_logistic_step_corner_cases(x, r, expected):
 def test_logistic_step_generic_cases(x, r, expected):
     result = logistic_step(x, r)
     assert_allclose(result, expected)
+
+@pytest.mark.parametrize('x, r, n_iter, expected', [
+        (0.1, 2.2, 1, [0.1, 0.198]),
+        (0.2, 3.4, 4, [0.2, 0.544, 0.843418, 0.449019, 0.841163]),
+        (0.5, 2, 3, [0.5, 0.5, 0.5, 0.5]),
+    ]
+    )
+def test_logistic_iterations(x, r, n_iter, expected):
+    result = run_iterations(x, r, n_iter)
+    assert_allclose(result, expected, rtol=1e-4)
