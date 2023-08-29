02ec3c21b9be3936ef26b387b68d538f66d1014c
ASPP Student <student@localhost>
2023-08-29 12:25:21 +0300

    Implementation of parametrization

--- test_logistic.py
+++ test_logistic.py
@@ -1,19 +1,19 @@
 from numpy.testing import assert_allclose
-
+import pytest
 from logistic import f
 
 # Add here your test for the logistic map
 
-def test_f_new_cases():
-    cases = [
+@pytest.mark.parametrize('x, r, expected', [
         (0.1, 2.2, 0.198),
         (0.2, 3.4, 0.544), 
         (0.5, 2, 0.5)
-    ]
-    for x, r, expected in cases:
-        result = f(x, r)
-        assert_allclose(result, expected)
-        
+    ])
+
+def test_f_generic_cases(x, r, expected):
+    result = f(x, r)
+    assert_allclose(result, expected)
+
 
 def test_f_corner_cases():
     # Test cases are (x, r, expected)
