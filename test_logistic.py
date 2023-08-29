b4ddad2f603ef3eaa809a3de78476873efc3152a
ASPP Student <student@localhost>
2023-08-29 12:33:08 +0300

    change test with parametrize

--- test_logistic.py
+++ test_logistic.py
@@ -1,27 +1,18 @@
 from numpy.testing import assert_allclose
-
+import pytest
 from logistic import f
 
 # Add here your test for the logistic map
 
+@pytest.mark.parametrize('x,r,expected', [(0, 1.1, 0),
+        (1, 3.7, 0),])
+def test_f_corner_cases(x,r,expected):
+    result = f(x, r)
+    assert_allclose(result, expected)
 
-def test_f_corner_cases():
-    # Test cases are (x, r, expected)
-    cases = [
-        (0, 1.1, 0),
-        (1, 3.7, 0),
-    ]
-    for x, r, expected in cases:
-        result = f(x, r)
-        assert_allclose(result, expected)
-
-def test_f_generic_cases():
-    # Test cases are (x, r, expected)
-    cases = [
-        (0.1, 2.2, 0.198),
+@pytest.mark.parametrize('x,r,expected', [(0.1, 2.2, 0.198),
         (0.2, 3.4, 0.544),
-        (0.5, 2, 0.5)
-    ]
-    for x, r, expected in cases:
-        result = f(x, r)
-        assert_allclose(result, expected)
+        (0.5, 2, 0.5)])
+def test_f_generic_cases(x,r,expected):
+    result = f(x, r)
+    assert_allclose(result, expected)
